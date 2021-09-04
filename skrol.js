aee4769c4fe1a1fac2bd960637c1592b2868add3
JurajFox <43251893+JurajFox@users.noreply.github.com>
2021-09-05 01:32:31 +0200

    naslovna layout

--- skrol.js
+++ skrol.js
@@ -1,6 +1,6 @@
 $(window).on("scroll", function () {
   var scrollPos = $(window).scrollTop();
-  if (scrollPos <= 500) {
+  if (scrollPos <= 300) {
     $(".navbar-custom").addClass("top");
   } else {
     $(".navbar-custom").removeClass("top");
